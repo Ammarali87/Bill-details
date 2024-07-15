2efe3ad22c258920e1f0f1ead89796363ec779ce
Amar Ibrahim <113317548+Ammarali87@users.noreply.github.com>
2024-07-16 02:56:10 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from "react"
-import "App.css"
- export default function App(){
+import './App.css';
+  export default function App(){
    return (
      <>
       <h2>helo world</h2>
      </>  
    )
  }
+
+
+
